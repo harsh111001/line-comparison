2a31d4f7552c0d6d8d1ebf4148026f7f0248337f
harsh <harshjain112001@gmail.com>
2023-01-18 15:23:45 +0530

    used oops concepts

--- line_comp/Program.cs
+++ line_comp/Program.cs
@@ -1,47 +1,22 @@
-﻿namespace line_comp
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace line_comp
 {
     internal class Program
     {
-        static int findlength(int x1,int x2,int y1,int y2)
-        {
-            return (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
-        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, This is line comparison program ");
-            int x1, x2, y1, y2;
-            int ctr = 1;
-            int firstline=0, secondline=0;
-            for(int i = 0; i < 2; i++)
-            {
-                Console.WriteLine("Enter values for line "+ctr+Environment.NewLine);
-                Console.WriteLine("Enter value for coordinate x1 ");
-                x1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter value for coordinate x2 ");
-                x2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter value for coordinate y1 ");
-                y1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter value for coordinate y2 ");
-                y2 = Convert.ToInt32(Console.ReadLine());
-                if(ctr==1 ) { firstline = findlength(x1, x2, y1, y2); }
-                else
-                {
-                    secondline = findlength(x1, x2, y1, y2);
-                }
-                ctr++;
-            }
-            //Console.WriteLine($"length of line1 is {firstline} and length of line2is {secondline}");
-            if (firstline == secondline)
-            {
-                Console.WriteLine("both the lines are equal");
-            }else if(secondline > firstline) 
-            {
-                Console.WriteLine("line1 is smaller than line2");
-            }
-            else
-            {
-                Console.WriteLine("line1 is greater than line2");
-            }
+            Line l1 = new Line(1, 1, 3, 4);
+            Line l2 = new Line(3, 5, 2, 2);
+            Line l3 = new Line(3, 4, 2, 2);
+            Console.WriteLine($"Length of l1 is {l1.length()}");
+            Console.WriteLine($"Length of l2 is {l2.length()}");
+            Console.WriteLine($"Length of l3 is {l3.length()}");
+            l1.compare(l2);
+            l2.compare(l1);
+            l1.equality(l2);
+            l3.equality(l1);
         }
     }
 }
---
+++ line_comp/line.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace line_comp
+{
+    internal class Line
+    {
+        private int x1;
+        private int x2;
+        private int y1;
+        private int y2;
+
+        public Line(int x1, int x2, int y1, int y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+        public double length()
+        {
+            return Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+        }
+        public void equality(Line other)
+        {
+            if(this.length() != other.length())
+            {
+                Console.WriteLine("Not Equal");
+            }
+            else
+            {
+                Console.WriteLine("Equal");
+            }
+        }
+        public void compare(Line other)
+        {
+            if(this.length()>other.length())
+            {
+                Console.WriteLine("This line is greater");
+            }
+            else if(this.length()<other.length())
+            {
+                Console.WriteLine("other line is greater");
+            }
+            else
+            {
+                Console.WriteLine("both are equal");
+            }
+        }
+
+    }
+}
